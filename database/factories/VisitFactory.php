ab94c11fcc4f15330311b847b21b15f832ae58a8
chang10th <chang10thomas@gmail.com>
2023-08-25 16:12:46 +0200

    création des migrations visit et role

---
+++ database/factories/VisitFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Visit>
+ */
+class VisitFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $date = $this->faker->dateTimeBetween('-20 years','-2 months')->format('Y-m-d');
+        return [
+            'date'=>$date,
+        ];
+    }
+}
